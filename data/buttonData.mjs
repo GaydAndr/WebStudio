d6c1423d0a93ce0c1b21ed1a1b7bb4ba0172aeef
andrii_h <galgayand@gmail.com>
2023-10-18 19:28:43 +0300

    create data file

---
+++ data/buttonData.mjs
@@ -0,0 +1,7 @@
+export const buttonData = [
+	{ text: "Усе", category: "all" },
+	{ text: "Веб-сайти", category: "веб-сайти" },
+	{ text: "Програми", category: "програми" },
+	{ text: "Дизайн", category: "дизайн" },
+	{ text: "Маркетинг", category: "маркетинг" },
+];
